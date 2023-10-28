520f351e1656b3bca251cc67eb491cb10a9bd991
YuSa0-6 <fuku.sugar@icloud.com>
2023-10-28 19:41:26 +0900

    finish fileupload to public directory

--- pages/PostFile.vue
+++ pages/PostFile.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <h1>Post File</h1>
+    <h1>ファイルアップロード</h1>
+    <form>
+      <label for="file">File: </label>
+      <input type="file" name="file" @change="onChange" />
+    </form>
+    <ul>
+      <li><img src="" style="width: 200px" /></li>
+    </ul>
   </div>
 </template>
+<script setup>
+const onChange = async (e) => {
+  const files = e.target.files;
+  const formData = new FormData();
+  formData.append('file', files[0]);
+  await useFetch('/api/upload', {
+    method: 'post',
+    body: formData,
+  });
+};
+</script>
